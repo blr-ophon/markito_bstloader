e884dd4216883a21cc497a3517045122a932058c
blrophon <gabriel.pequod@gmail.com>
2023-03-25 17:19:32 -0300

    Implement PIC instructions

--- src/kernel/pic/pic.c
+++ src/kernel/pic/pic.c
@@ -1,8 +1,100 @@
 #include "pic/pic.h"
 
-void PIC_init(void){
+void PIC_init(uint8_t Mpic_offset, uint8_t Spic_offset){
+    //ICW1
     ior_outb(MPIC_CTRL_PORT, ICW1_ICW4 | ICW1_INIT);
     ior_wait();
     ior_outb(SPIC_CTRL_PORT, ICW1_ICW4 | ICW1_INIT);
     ior_wait();
+
+    //ICW2
+    ior_outb(MPIC_DATA_PORT, Mpic_offset); 
+    ior_wait();
+    ior_outb(SPIC_DATA_PORT, Spic_offset); 
+    ior_wait();
+
+    //ICW3
+    ior_outb(MPIC_DATA_PORT, 0x4);      //slave at IRQ2
+    ior_wait();
+    ior_outb(SPIC_DATA_PORT, 0x2);      //cascade identity
+    ior_wait();
+
+    //ICW4
+    ior_outb(MPIC_DATA_PORT, ICW4_8086);      //8086 mode
+    ior_wait();
+    ior_outb(SPIC_DATA_PORT, ICW4_8086);      //8086 mode
+    ior_wait();
+
+    //clear 
+    ior_outb(MPIC_DATA_PORT, 0);      //8086 mode
+    ior_wait();
+    ior_outb(SPIC_DATA_PORT, 0);      //8086 mode
+    ior_wait();
+
+}
+
+
+void PIC_Mask(int irq_x){
+    //Masks(disable) irqx 
+    //Reads mask register from data port, sets the corresponding
+    //mask bit and writes it back
+
+    if(irq_x < 8){ //Master PIC
+        uint8_t mask = ior_inb(MPIC_DATA_PORT);
+        ior_outb(MPIC_DATA_PORT, mask | (1 << irq_x));
+    }else{ //Slave PIC
+        irq_x -= 8;     
+        uint8_t mask = ior_inb(SPIC_DATA_PORT);
+        ior_outb(SPIC_DATA_PORT, mask | (1 << irq_x));
+    }
+}
+
+void PIC_unMask(int irq_x){
+    //unmasks(enable) irqx 
+    //Reads mask register from data port, clear the corresponding
+    //mask bit and writes it back
+
+    if(irq_x < 8){ //Master PIC
+        uint8_t mask = ior_inb(MPIC_DATA_PORT);
+        ior_outb(MPIC_DATA_PORT, mask & ~(1 << irq_x));
+    }else{ //Slave PIC
+        irq_x -= 8;     
+        uint8_t mask = ior_inb(SPIC_DATA_PORT);
+        ior_outb(SPIC_DATA_PORT, mask & ~(1 << irq_x));
+    }
+}
+
+void PIC_Disable(void){
+    //If using APIC instead of cascaded 8259 PICs
+    ior_outb(MPIC_DATA_PORT, 0xff);
+    ior_wait();
+    ior_outb(SPIC_DATA_PORT, 0xff);
+    ior_wait();
+}
+
+void PIC_Send_EOI(int irq){
+    if(irq >= 8){
+        ior_outb(SPIC_CTRL_PORT, OCW2_EOI);
+    }
+    ior_outb(MPIC_CTRL_PORT, OCW2_EOI);
+}
+
+uint16_t PIC_readIRR(void){
+    //Send READ_IRR command word, read lower and high byte from ctrl port
+    ior_outb(MPIC_CTRL_PORT, OCW2_READ_IRR);
+    ior_wait();
+    ior_outb(SPIC_CTRL_PORT, OCW2_READ_IRR);
+    ior_wait();
+    //litle endian
+    return ior_inb(SPIC_CTRL_PORT) | (ior_inb(SPIC_CTRL_PORT) << 8);
+}
+
+uint16_t PIC_readISR(void){
+    //Send READ_ISR command word, read lower and high byte from ctrl port
+    ior_outb(MPIC_CTRL_PORT, OCW2_READ_ISR);
+    ior_wait();
+    ior_outb(SPIC_CTRL_PORT, OCW2_READ_ISR);
+    ior_wait();
+    //litle endian
+    return ior_inb(SPIC_CTRL_PORT) | (ior_inb(SPIC_CTRL_PORT) << 8);
 }
--- src/kernel/pic/pic.h
+++ src/kernel/pic/pic.h
@@ -8,6 +8,10 @@
 #define SPIC_CTRL_PORT 0xa0
 #define SPIC_DATA_PORT 0xa1
 
+/*
+ * PIC required 4 initialization command words (ICW1-4), with 3 and 4 being optional.
+ */
+
 enum{
     ICW1_ICW4       = 0x01,         //set if ICW4 is needed before initialization (see datasheet)
     ICW1_SINGLE     = 0x02,         //single/cascade mode
@@ -15,5 +19,22 @@ enum{
     ICW1_LEVEL      = 0x08,         //edge/level triggered mode
     ICW1_INIT       = 0x10          //initialize PIC
 } PIC_ICW1;
+
+enum{
+    ICW4_8086               = 0x01,         //(1)8086/8088 Mode or (0)MCS 80/85 MODE
+    ICW4_AUTO_EOI           = 0x02,         //auto or normal end of interrupt
+    ICW4_BUFFERED_SLAVE     = 0x08, //Not important for x86
+    ICW4_BUFFERED_MASTER    = 0x0c, //Not important for x86 
+    ICW4_FULLY_NESTED       = 0x10  //Not important for x86
+} PIC_ICW4;
+
+//OPERATION COMMAND WORDS
+//after initialization, 8259 can accept interrupts and be modified via OCWs
+//OCW1 deals with masks
+enum{
+    OCW2_EOI        = 0x20,         //End Of Interrupt
+    OCW2_READ_IRR   = 0x0a,         //IRQ Request Register
+    OCW2_READ_ISR   = 0x0a          //InService Register
+} PIC_OCW2;
 
 #endif
